d36b366b8d442bda6e4648cc8ce567af699dc84d
Oleksandr <93466475+oleksandrmyhydiuk@users.noreply.github.com>
2026-04-15 19:31:02 +0300

    feat: add test data

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,10 +1,12 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
+import process from "process";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
+    seed: "npx ts-node prisma/seed.ts",
   },
   datasource: {
     url: process.env.DATABASE_URL,
