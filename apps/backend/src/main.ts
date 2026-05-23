7b88a22d111dc22d62180a8d83ec591ec25924aa
sofiiahaman <sofiia.haman@gmail.com>
2026-05-23 14:42:10 +0300

    chore: replace default nest logger with winston

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from "@nestjs/core";
+import { WINSTON_MODULE_NEST_PROVIDER } from "nest-winston";
 import { AppModule } from "./app.module";
 import { ApiDocumentationService } from "./shared/api-documentation.service";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
 
   ApiDocumentationService.configure(app);
 
