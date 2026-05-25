7ca471cd4670ee0fce889c791698898ded2faaed
sofiiahaman <sofiia.haman@gmail.com>
2026-05-25 18:37:07 +0300

    chore: install better-auth and configure environment

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,11 +6,17 @@ import { ApiDocumentationService } from "./shared/api-documentation.service";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true,
+    bodyParser: false,
   });
 
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
 
   ApiDocumentationService.configure(app);
+
+  app.enableCors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  });
 
   await app.listen(process.env.PORT ?? process.env.port ?? 3000);
 }
